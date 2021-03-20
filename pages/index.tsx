8b7a7556edd2abf65dbc8fff742e8d880286044f
Masato Arai <masato@ntslive.co.uk>
2021-03-20 21:11:56 +0000

    disable data not found

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,14 @@ import Layout from '../components/Layout'
 import { request } from '../lib/datocms'
 
 import GET_HOME_DATA from '../graphql/query/get-home-data'
-import DataNotFound from '../components/DataNotFound'
+// import DataNotFound from '../components/DataNotFound'
 
 interface HomeProps {
   data: object;
 }
 
 const Home = ({ data }) => {
-  if (!data) return <DataNotFound />
+  // if (!data) return <DataNotFound />
 
   return (
     <Layout title="Masato Arai - Web App Developer">
