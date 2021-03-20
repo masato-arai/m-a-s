196d9bed52c589c8969e86d9f47ecab06063e4dc
Masato Arai <masato@ntslive.co.uk>
2021-03-20 01:35:59 +0000

    add linebreak

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
+
 import client from '../lib/apollo-client'
 
 function MyApp({ Component, pageProps }: AppProps) {
