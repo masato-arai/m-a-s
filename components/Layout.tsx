173bfd2d6c06aeb4c0889b80e488abef6d1b57fc
Masato Arai <masato@ntslive.co.uk>
2021-03-24 22:47:22 +0000

    add image asset

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,11 +18,11 @@ const Layout = ({ children, title = 'Masato Arai - Web App Developer' }: Props) 
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Masato Arai - Web App Developer" />
 
-        <meta name="image" content="https://m-a-s.info/static/images/home.jpg" />
+        <meta name="image" content="https://m-a-s.info/images/home.jpg" />
 
         <meta name="og:title" content="Masato Arai" />
         <meta name="og:description" content="Masato Arai - Web App Developer" />
-        <meta name="og:image" content="https://m-a-s.info/static/images/home.jpg" />
+        <meta name="og:image" content="https://m-a-s.info/images/home.jpg" />
         <meta name="og:url" content="https://m-a-s.info" />
         <meta name="og:site_name" content="Masato Arai" />
         <meta name="og:locale" content="en_GB" />
