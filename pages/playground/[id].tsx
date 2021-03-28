10e92a2e9dd5e5f166a3d80dce042be63c3e540c
Masato Arai <masato@ntslive.co.uk>
2021-03-28 20:37:46 +0100

    add layout to posts

--- pages/playground/[id].tsx
+++ pages/playground/[id].tsx
@@ -1,13 +1,31 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
+
+import Layout from '../../components/Layout'
+import Header from '../../components/Header/Header'
+
+import { Wrapper } from '../../styles/utils/wrappers'
 
 const Post = () => {
   const router = useRouter()
   const { id } = router.query
 
   return (
-    <>
-      <h1>Post: {id}</h1>
-    </>
+    <Layout>
+      <Header />
+
+      <main>
+        <Wrapper>
+          <div>
+            <Link href="/playground" passHref>
+              <a>Back to playground</a>
+            </Link>
+          </div>
+
+          <h1>Post: {id}</h1>
+        </Wrapper>
+      </main>
+    </Layout>
   )
 }
 
--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -4,27 +4,32 @@ import Link from 'next/link'
 import Layout from '../../components/Layout'
 import Header from '../../components/Header/Header'
 
+import { Wrapper } from '../../styles/utils/wrappers'
+
 const Playground = () => {
   const router = useRouter()
-  const { id } = router.query
 
   return (
     <Layout>
       <Header />
 
-      <h1>Playground</h1>
-      <ul>
-        <li>
-          <Link href="/playground/[id]" as={`/playground/first-post`}>
-            <a>First post</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/playground/[id]" as={`/playground/second-post`}>
-            <a>Second post</a>
-          </Link>
-        </li>
-      </ul>
+      <main>
+        <Wrapper>
+          <h1>Playground</h1>
+          <ul>
+            <li>
+              <Link href="/playground/[id]" as={`/playground/first-post`}>
+                <a>First post</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/playground/[id]" as={`/playground/second-post`}>
+                <a>Second post</a>
+              </Link>
+            </li>
+          </ul>
+        </Wrapper>
+      </main>
     </Layout>
   )
 }
